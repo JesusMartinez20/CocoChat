93b553ac7120bacd42d2a3db37aedcce6416f4d5
XV02 <lghhssi@gmail.com>
2019-05-20 15:40:01 -0500

    Recieve solicitudes amigos

---
+++ src/cocochat/Solicitudes_Amigos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cocochat;
+
+/**
+ *
+ * @author lghhs
+ */
+public class Solicitudes_Amigos {
+    int id;
+    String nombre;
+
+    public Solicitudes_Amigos(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Solicitudes_Amigos() {
+    }  
+}
